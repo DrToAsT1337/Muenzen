5b9675f95df6850336a20eac5334a557d39b1f19
DrToAsT1337 <erikvogt97@gmail.com>
2026-08-19 16:44:25 +0200

    erster rudimentärer Aufruf

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,13 @@
+<script setup>
+// Hier rufen wir unsere neue API-Route ab
+const { data: meineMuenzen } = await useFetch('/api/euroset')
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Willkommen im Münz-Archiv! 🪙</h1>
+    
+    <!-- Das "pre" Tag zeigt die Rohdaten schön formatiert an -->
+    <pre>{{ meineMuenzen }}</pre>
   </div>
 </template>
